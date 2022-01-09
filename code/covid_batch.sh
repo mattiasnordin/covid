647c7a6ba8e8f4d4148216acfe77a15b70076f89
Mattias Nordin <mnordin84@gmail.com>
2022-01-09 20:50:03 +0100

    made changes

---
+++ code/covid_batch.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+echo on
+set root=D:\covid
+call "C:\Users\menor\AppData\Local\Continuum\anaconda3\Scripts\activate.bat"
+CD /D %root%
+python -W ignore "code\covid_intl.py"
+git checkout dev
+git add .
+git commit -m "Automatic commit"
+git push
+echo Press Enter...
+read
