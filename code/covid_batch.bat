82aa61d4abb23459da315746021684d287cfd731
Mattias Nordin <mnordin84@gmail.com>
2022-01-09 20:54:30 +0100

    Automatic commit

--- code/covid_batch.bat
+++ code/covid_batch.bat
@@ -4,7 +4,6 @@ set root=D:\covid
 call "C:\Users\menor\AppData\Local\Continuum\anaconda3\Scripts\activate.bat"
 CD /D %root%
 python -W ignore "code\covid_intl.py"
-git checkout dev
 git add .
 git commit -m "Automatic commit"
 git push
--- code/covid_batch.sh
+++ code/covid_batch.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
-echo on
-set root=D:\covid
-call "C:\Users\menor\AppData\Local\Continuum\anaconda3\Scripts\activate.bat"
-CD /D %root%
-python -W ignore "code\covid_intl.py"
-git checkout dev
+cd "D:\covid"
 git add .
-git commit -m "Automatic commit"
+git commit -am "Automatic commit"
 git push
-echo Press Enter...
-read
