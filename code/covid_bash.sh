6cc75c8b0cd7520a891cec74561a2b517cb90719
mattiasnordin <mnordin84@gmail.com>
2022-02-17 18:49:39 +0100

    Automatic commit

--- code/covid_bash.sh
+++ code/covid_bash.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 git add .
+git config --global user.email "mnordin84@gmail.com"
+git config --global user.name "mattiasnordin"
 git commit -am "Automatic commit"
 git push
--- code/covid_batch.bat
+++ code/covid_batch.bat
@@ -1,6 +1,5 @@
 echo on
 set root=D:\covid
-call "C:\Users\menor\AppData\Local\Continuum\anaconda3\Scripts\activate.bat"
 CD /D %root%
 python -W ignore "code\covid_intl.py"
 timeout 5
